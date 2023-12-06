fcdf1388765151c661cd609363cac58db6ee565d
PedroDev <pedrodev0202@gmail.com>
2023-12-06 08:57:06 -0300

    commit

---
+++ src/ex.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+function saudacoes(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+multiplicacao(2, 4);
+saudacoes('Pedro');
---
+++ src/ex.ts
@@ -0,0 +1,10 @@
+function multiplicacao(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+function saudacoes(nome: string){
+    return `Olá ${nome}`
+}
+
+multiplicacao(2, 4);
+saudacoes('Pedro');
